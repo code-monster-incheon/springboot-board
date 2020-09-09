8e5f8fdd7b79cd9a609b39b01cbcd23a9773db73
sunny <ishong@inu.ac.kr>
2020-09-09 20:16:24 +0900

    상품 주문하기 기본 API 개발

---
+++ src/main/java/com/algo/inc/web/controller/view/order/OrderViewController.java
@@ -0,0 +1,34 @@
+package com.algo.inc.web.controller.view.order;
+
+import com.algo.inc.domain.product.Product;
+import com.algo.inc.web.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/view/order")
+public class OrderViewController {
+
+    private final ProductService productService;
+
+    @GetMapping("/speedorder")
+    public String getOrderView(Long id, Model model)
+    {
+        Product product = productService.getProductDetail(id);
+        model.addAttribute("vo", product);
+        return "/order/speedorder";
+    }
+
+    @GetMapping("/ordercomplete")
+    public String getCompelte(){
+        return "/order/ordercomplete";
+    }
+
+
+}
