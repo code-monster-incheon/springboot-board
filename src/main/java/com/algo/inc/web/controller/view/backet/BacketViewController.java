a9a21fc889a17aa7c04ae44975a8f202e194ef3b
ysh <wlatmdqja@naver.com>
2020-09-19 01:11:17 +0900

    장바구니 및 시큐리티 적용

--- src/main/java/com/algo/inc/web/controller/view/backet/BacketViewController.java
+++ src/main/java/com/algo/inc/web/controller/view/backet/BacketViewController.java
@@ -1,4 +1,19 @@
 package com.algo.inc.web.controller.view.backet;
 
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basket")
+@Log
 public class BacketViewController {
+
+    @GetMapping("/my-basket")
+    public void mybasket()
+    {
+        log.info("my-basket");
+    }
+
 }
